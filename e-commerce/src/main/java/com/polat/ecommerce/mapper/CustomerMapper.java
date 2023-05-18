7182247dae7ab7acc208465132151aea42e11269
Polat <mustfplt4@gmail.com>
2023-05-18 17:45:58 +0300

    customer mapper added

---
+++ e-commerce/src/main/java/com/polat/ecommerce/mapper/CustomerMapper.java
@@ -0,0 +1,23 @@
+package com.polat.ecommerce.mapper;
+
+import com.polat.ecommerce.dto.CustomerDTO;
+import com.polat.ecommerce.dto.CustomerSaveRequest;
+import com.polat.ecommerce.model.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CustomerMapper {
+
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
+    Customer convertToCustomer(CustomerSaveRequest customerSaveRequest);
+
+    CustomerDTO convertToCustomerDTO(Customer customer);
+
+    List<CustomerDTO> convertToCustomerDTOList (List<Customer> customers);
+
+}
