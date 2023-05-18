cac1d111404c978cddde497733d38a4b374b828d
Polat <mustfplt4@gmail.com>
2023-05-18 17:44:31 +0300

    findByName added to CustomerRepository

--- e-commerce/src/main/java/com/polat/ecommerce/repository/CustomerRepository.java
+++ e-commerce/src/main/java/com/polat/ecommerce/repository/CustomerRepository.java
@@ -3,5 +3,9 @@ package com.polat.ecommerce.repository;
 import com.polat.ecommerce.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByName(String name);
 }
