446734c6c61faa4f95987da1c7be8ed68639dfcd
Polat <mustfplt4@gmail.com>
2023-05-21 11:59:19 +0300

    added swagger

---
+++ e-commerce/src/main/java/com/polat/ecommerce/general/SpringdocConfig.java
@@ -0,0 +1,16 @@
+package com.polat.ecommerce.general;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+    @Bean
+    public OpenAPI OpenApiBase() {
+        return new OpenAPI().info(new Info().title("hw2"));
+    }
+}
